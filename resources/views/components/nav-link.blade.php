dedccfe333152b993345eeb702b8fcda64ccdfeb
d3ric3 <d3ric3@gmail.com>
2024-06-15 11:08:25 +0800

    EP06 progress 3:00

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -14,3 +14,17 @@ aria-current="{{ $active ? 'page' : 'false' }}"
 >
 {{ $slot }}
 </a>
+
+<!-- Sample for rendering anchor or button based on 'type' prop
+@props(['active' => false, type => 'a'])
+
+<php if($type == 'a') : ?>
+    <a class"{{ $active ? 'active-style' : 'inactive-style' }} {{ $attributes }}>{{ $slot }}</a>
+<php else : ?>
+    <button class"{{ $active ? 'active-style' : 'inactive-style' }} {{ $attributes }}>{{ $slot }}</button>
+<php endif ?>
+
+############## Usage: layout.blade.php ##############
+<x-nav-link :active='false' type='button'>This is a button</x-nav-link>
+<x-nav-link :active='false' type='a'>This is an anchor</x-nav-link>
+-->
